4da3c68a53fefa6a03e520637986b857cc993fed
Laura Amancio <lauran.amancio@yahoo.com.br>
2022-07-11 12:11:57 -0300

    custom error

--- src/data/BaseDatabase.ts
+++ src/data/BaseDatabase.ts
@@ -1,17 +1,17 @@
-import knex from "knex"
-import dotenv from "dotenv"
+import knex from "knex";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export class BaseDatabase {
-    protected static connection = knex({
-        client: "mysql",
-        connection: {
-            host: process.env.DB_HOST,
-            port: 3306,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB_NAME
-        }
-    })
+  protected static connection = knex({
+    client: "mysql",
+    connection: {
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+  });
 }
--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,12 @@ import BikeController from "./controller/BikeController";
 import BikeDatabase from "./data/BikeDatabase";
 import { IdGenerator } from "./services/IdGenerate";
 
-const bikeBusiness = new BikeBusiness(
-    new IdGenerator(),
-    new BikeDatabase()
-)
-const bikeController = new BikeController(bikeBusiness)
+const bikeBusiness = new BikeBusiness(new IdGenerator(), new BikeDatabase());
+const bikeController = new BikeController(bikeBusiness);
 
-app.post("/bikes/cadastro", bikeController.addBike)
-app.delete("/bikes/:id", bikeController.deleteBike)
-app.put("/bikes/detalhes/:id", bikeController.changePrice)
-app.get("/bikes", bikeController.getAllProducts)
-app.get("/bikes/:color", bikeController.getByColor)
-app.get("/bikes/valores/crescente", bikeController.getByValue)
+app.post("/bikes/cadastro", bikeController.addBike);
+app.delete("/bikes/:id", bikeController.deleteBike);
+app.put("/bikes/detalhes/:id", bikeController.changePrice);
+app.get("/bikes", bikeController.getAllProducts);
+app.get("/bikes/:color", bikeController.getByColor);
+app.get("/bikes/valores/crescente", bikeController.getByValue);
